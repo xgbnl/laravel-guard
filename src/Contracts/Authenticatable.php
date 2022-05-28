843ec18609fd33d93494c226baa29087ffc0560a
xgbnl <745502274@qq.com>
2022-05-28 17:29:54 +0800

    feat: 新增接口文件

--- src/Contracts/Authenticatable.php
+++ src/Contracts/Authenticatable.php
@@ -4,5 +4,25 @@ namespace Xgbnl\Bearer\Contracts;
 
 interface Authenticatable
 {
+    /**
+     * Get the name of the unique identifier for the user.
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName(): string;
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier(): mixed;
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string|null
+     */
+    public function getAuthPassword(): ?string;
 
 }
--- src/Contracts/Provider/Provider.php
+++ src/Contracts/Provider/Provider.php
@@ -1,8 +1,23 @@
 <?php
 
-namespace Xgbnl\Bearer\Contracts;
+namespace Xgbnl\Bearer\Contracts\Provider;
+
+use Xgbnl\Bearer\Contracts\Authenticatable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 
 interface Provider
 {
+    /**
+     * 按ID检索并获取用户
+     * @param int $id
+     * @return Model|Authenticatable|null
+     */
+    public function retrieveById(int $id): Authenticatable|Model|null;
 
+    /**
+     * 解析返回一个构造查询器
+     * @return Builder
+     */
+    public function resolve():Builder;
 }
