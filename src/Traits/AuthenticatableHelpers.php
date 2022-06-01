f6ebc79587d266884b1ce801bc6abbcebbca2956
xgbnl <745502274@qq.com>
2022-06-01 14:38:33 +0800

    chore: 更改模块名称

---
+++ src/Traits/AuthenticatableHelpers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Xgbnl\Bearer\Traits;
+
+trait AuthenticatableHelpers
+{
+    /**
+     * 获取用户唯一标识符名称
+     * @return string
+     */
+    public function getModelIdentifierName(): string
+    {
+        return 'id';
+    }
+
+    /**
+     * 获取用户唯一标识符
+     * @return int
+     */
+    public function getModelIdentifier(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * 获取用户的密码
+     * @return string|null
+     */
+    public function getModelPassword(): ?string
+    {
+        return $this->password ?? null;
+    }
+}
