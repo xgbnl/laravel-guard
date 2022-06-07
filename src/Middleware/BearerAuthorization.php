c520c6946c78e710e228660ca987a1c66da2cbef
xgbnl <745502274@qq.com>
2022-06-07 21:41:41 +0800

    chore: 优化中间件

--- src/Middleware/BearerAuthorization.php
+++ src/Middleware/BearerAuthorization.php
@@ -4,22 +4,17 @@ declare(strict_types=1);
 
 namespace Xgbnl\Bearer\Middleware;
 
-use Xgbnl\Bearer\Exception\BearerException;
-
 class BearerAuthorization extends Authorization
 {
-    /**
-     * @throws BearerException
-     */
     public function doHandle()
     {
 
         if ($this->guard()->guest()) {
-            throw new BearerException('请登录后再试', 403);
+            trigger(403,'请登录后重试');
         }
 
         if ($this->guard()->expires()) {
-            throw new BearerException('令牌已失效,请重新登录', 403);
+           trigger(403,'令牌已失效,请重新登录');
         }
 
     }
