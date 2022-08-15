b0feca544080eb46f412bcc454d7cc8e7415a341
xgbnl <745502274@qq.com>
2022-08-15 17:26:38 +0800

    chore: 更改中间件状态码

--- src/Middleware/BearerAuthorization.php
+++ src/Middleware/BearerAuthorization.php
@@ -9,11 +9,11 @@ class BearerAuthorization extends Authorization
     public function doHandle()
     {
         if ($this->guard()->guest()) {
-            trigger(403, '无效令牌访问,请登录');
+            trigger(401, '无效令牌访问,请登录');
         }
 
         if ($this->guard()->expires()) {
-            trigger(403, '令牌已过期,请重新登录');
+            trigger(401, '令牌已过期,请重新登录');
         }
     }
 }
